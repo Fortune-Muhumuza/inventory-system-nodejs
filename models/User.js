5044a570fc62c0ad5d6f2b9909b6e37c679d119c
fortune-muhumuza <fortunemuhumuza@gmail.com>
2020-10-11 23:08:24 -0700

    made the profit total function to work again

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 
-var SALT_FACTOR = 10;
 
 // User Schema
 const UserSchema = mongoose.Schema({
@@ -15,37 +14,6 @@ const UserSchema = mongoose.Schema({
     required: true,
   },
 });
-
-UserSchema.methods.name = function() {
-  return this.displayName || this.username;
-}
-
-var noop = function() {};
-
-UserSchema.pre('save', function(done) {
-  var user = this;
-  if(!user.isModified('password')) {
-      return done();
-  }
-  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
-      if(err) { 
-          return done(err) 
-      }
-      bcrypt.hash(user.password, salt, noop, function(err, hashedPassword) {
-          if(err) {
-              return done(err);
-          }
-          user.password = hashedPassword;
-          done();
-      });
-  });
-});
-
-UserSchema.methods.checkPassword = function(guess, done) {
-  bcrypt.compare(guess, this.password, function(err, isMatch) {
-      done(err, isMatch);
-  });
-}
 
 // eslint-disable-next-line no-unused-vars
 const User = (module.exports = mongoose.model('User', UserSchema));
