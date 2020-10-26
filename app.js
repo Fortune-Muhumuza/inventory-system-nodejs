cafa331ef83ca426db0f0d8203fa371d5dfa498a
fortune-muhumuza <fortunemuhumuza@gmail.com>
2020-10-26 09:35:32 -0700

    improved the styling of the program focusing on the tables

--- app.js
+++ app.js
@@ -17,6 +17,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(
   session({
     name: 'sid',
