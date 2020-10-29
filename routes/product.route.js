a54d884a21425705d1505db434a29987f1f98a0b
fortune-muhumuza <fortunemuhumuza@gmail.com>
2020-10-29 04:16:21 -0700

    commented about some routes needing to be changed to post method

--- routes/product.route.js
+++ routes/product.route.js
@@ -15,6 +15,7 @@ router.get('/singleSaleRecord/:id', checkToken.checkAuth, product_controller.dis
 
 router.post('/sell', checkToken.checkAuth, product_controller.product_sell1);
 router.post('/buy', checkToken.checkAuth, product_controller.product_buy);
+//some of these may need to be changed to post method
 router.get('/deleteRecord/:id', checkToken.checkAuth, product_controller.deleteStoreRecord);
 router.get('/deleteSaleRecord/:id', checkToken.checkAuth, product_controller.deleteSaleRecord)
 
