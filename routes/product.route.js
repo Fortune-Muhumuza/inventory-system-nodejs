4d7f418cfe7256d474a6ea1ea9663fb255723c4b
fortune-muhumuza <fortunemuhumuza@gmail.com>
2020-10-28 21:11:25 -0700

    added route to get single sale record

--- routes/product.route.js
+++ routes/product.route.js
@@ -11,10 +11,12 @@ router.get('/sellTransactions', checkToken.checkAuth, product_controller.display
 router.get('/sellTransactionsJSON', product_controller.displayTransactionsJSON);
 router.get('/permanentRecords', checkToken.checkAuth, product_controller.getPermanentRecords)
 router.get('/singleProduct/:id', checkToken.checkAuth, product_controller.displaySingleProduct)
+router.get('/singleSaleRecord/:id', checkToken.checkAuth, product_controller.displaySingleSaleRecord)
 
 router.post('/sell', checkToken.checkAuth, product_controller.product_sell1);
 router.post('/buy', checkToken.checkAuth, product_controller.product_buy);
 router.get('/deleteRecord/:id', checkToken.checkAuth, product_controller.deleteStoreRecord);
+router.get('/deleteSaleRecord/:id', checkToken.checkAuth, product_controller.deleteSaleRecord)
 
 
 
