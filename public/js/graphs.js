2fca500192fc06f6ce718de0d87765aa055e94b4
fortune-muhumuza <fortunemuhumuza@gmail.com>
2020-12-20 09:52:10 -0800

    added doughnut chart type to store

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -55,6 +55,44 @@ function generateStoreGraph() {
     },
     data: [
       {
+        type: 'doughnut',
+        yValueFormatString: '#,### Units',
+        dataPoints: dataPoints,
+      },
+    ],
+  });
+  function addData(data) {
+    for (var i = 0; i < data.length; i++) {
+      console.log(data[i].name);
+      dataPoints.push({
+        label: data[i].name,
+        y: data[i].quantity,
+      });
+    }
+    chart.render();
+  }
+  $.getJSON('http://localhost:3000/storedatajson', addData);
+}
+
+function generateStoreGraph2() {
+  var dataPoints = [];
+  var chart = new CanvasJS.Chart('chartContainer', {
+    animationEnabled: true,
+    theme: 'light2',
+    title: {
+      text: 'Store data',
+    },
+    axisY: {
+      title: 'Quantity',
+      titleFontSize: 24,
+      includeZero: true,
+    },
+    axisX: {
+      title: 'Name',
+      titleFontSize: 24,
+    },
+    data: [
+      {
         type: 'column',
         yValueFormatString: '#,### Units',
         dataPoints: dataPoints,
